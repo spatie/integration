f47e5a76ca843f7636259d69afb1a042365aa614
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2015-06-19 13:53:34 +0200

    Bugfixes

--- src/TestCase.php
+++ src/TestCase.php
@@ -3,9 +3,9 @@
 namespace Spatie\Integration;
 
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-class TestCase extends TestCase
+class TestCase extends BaseTestCase
 {
     /**
      * The base URL to use while testing the application.
